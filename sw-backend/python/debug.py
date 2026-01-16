e05c686423b05e480fea7835ec9e4a556a802295
Harshath <harshathg2006@gmail.com>
2026-01-16 22:45:30 +0530

    Remove backend-rural and sw-backend from tracking

--- sw-backend/python/debug.py
+++
@@ -1,3 +0,0 @@
-import pickle
-with open("D:/Ayu-int/AyuSahayak/sw-backend/models/wound_class_names.pkl","rb") as f:
-    print(pickle.load(f))
--- sw-backend/python/test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-
-MODEL_PATH = "C:/Users/Venka/OneDrive/Desktop/medical_ai/medical_ai/backend/models/wound_model.h5"
-
-model = load_model(MODEL_PATH)
-model.summary()
